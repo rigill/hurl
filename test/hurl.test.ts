a61cee0d9756be57ff853e80cd83ff9768ebe2de
Richard Cargill <rcargill@dunelm.com>
2022-04-11 20:27:02 +0100

    Initial commit

---
+++ test/hurl.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Hurl from '../lib/hurl-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hurl-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Hurl.HurlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
