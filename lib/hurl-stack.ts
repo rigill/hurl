88a1f8828737856304d9a5c8821378bd05596b2a
Richard Cargill <rcargill@dunelm.com>
2022-04-12 11:58:52 +0100

    Using NodeJs lambda to avoid having to build with tsc

--- lib/hurl-stack.ts
+++ lib/hurl-stack.ts
@@ -1,24 +1,21 @@
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
-import * as cdk from 'aws-cdk-lib';
-import * as lambda from 'aws-cdk-lib/aws-lambda';
-import * as path from 'path';
+import * as lambda from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import {Construct} from 'constructs';
-import {Stack, StackProps} from 'aws-cdk-lib';
+import {Stack, StackProps, RemovalPolicy} from 'aws-cdk-lib';
 
 export class HurlStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const bucket = new s3.Bucket(this, 'MyBucket', {
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      removalPolicy: RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
 
-    const fn = new lambda.Function(this, 'MyFunction', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../src/')),
+    const fn = new lambda.NodejsFunction(this, 'MyFunction', {
+      handler: 'handler',
+      entry: './src/index.ts',
       environment: {
         BUCKET_NAME: bucket.bucketName,
       },
